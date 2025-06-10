1dbc13a373a28bec9fbaf5333ce7b6d63bb7d6e0
Gityashu <panduyashu45@gmail.com>
2025-06-10 15:35:15 +0530

    second commit

---
+++ terraform_eks/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-state-bucket-inv"        # <= EXACTLY MATCH YOUR S3 BUCKET NAME
+    key    = "ec2-deployment/terraform.tfstate" # <= Path within the bucket
+    region = "us-east-1"                        # <= EXACTLY MATCH YOUR AWS_REGION SECRET
+    #dynamodb_table = "terraform-lock-table"                      # <= EXACTLY MATCH YOUR DYNAMODB TABLE NAME
+    encrypt = true
+  }
+}
