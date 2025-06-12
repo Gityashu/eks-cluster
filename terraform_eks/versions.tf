35797add7f81485d43732d50d117cf6fbd072c86
Gityashu <panduyashu45@gmail.com>
2025-06-12 23:03:02 +0530

    Update versions.tf

--- terraform_eks/versions.tf
+++ terraform_eks/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "terraform-state-bucket-inv"        # <= EXACTLY MATCH YOUR S3 BUCKET NAME
+    bucket = "terraform-state-bucket-2f"        # <= EXACTLY MATCH YOUR S3 BUCKET NAME
     key    = "ec2-deployment/terraform.tfstate" # <= Path within the bucket
     region = "us-east-1"                        # <= EXACTLY MATCH YOUR AWS_REGION SECRET
     #dynamodb_table = "terraform-lock-table"                      # <= EXACTLY MATCH YOUR DYNAMODB TABLE NAME
